9f5646b84f5a0c1c0bf82d56eaa7a5fc376956aa
Purch1 <ucheprince26@gmail.com>
2023-09-29 19:20:03 +0100

    Granted staff access to get all product

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import productController from '../controllers/product.controller.js';
-import { verifyStaff, verifyManager, verifyOwner, verifySuperAdmin } from '../middleware/auth.verifiyToken.js';
+import { verifyToken, verifyStaff, verifyManager, verifyOwner, verifySuperAdmin } from '../middleware/auth.verifiyToken.js';
 
 const productRouter = express.Router();
 
 productRouter.post('/add', verifyStaff, productController.createProduct);
 productRouter.get('/get/:id', verifyStaff, productController.getOne);
-productRouter.get('/getAll', verifyManager, productController.getAll);
+productRouter.get('/getAll', verifyStaff, productController.getAll);
 productRouter.patch('/update/:id', verifyManager, productController.updateProductItem);
 productRouter.delete('/delete/:id', verifyOwner, productController.delete);
 
